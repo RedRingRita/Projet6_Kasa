a416cc5ce047c76e8b074ca3244fb59f4ae913d3
Prunelle <zhavina@protonmail.com>
2023-12-29 18:08:26 +0100

    Animation du collapse OK. Réglages des marges de la page d'accueil

--- src/Components/Collapse/Collapse.js
+++ src/Components/Collapse/Collapse.js
@@ -1,11 +1,36 @@
 import "./styleCollapse.css"
 import arrow from "../../assets/fleche.png"
+import { useState } from "react"
 
-function Collapse({text}){
+function Collapse({title, text}){
+
+    const [statut, setStatut] = useState("Fermée");
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return(
-        <div className="bar">
-            <div>{text}</div>
-            <img src={arrow} alt="flèche déroulante"></img>
+        <div className="blockCollapse">
+            <div className="blockCollapse__redbar">
+                <div>{title}</div>
+                <div>{statut}</div>
+            </div>
+                <img onClick={() => {
+                    // if (isOpen === false){
+                    //     setIsOpen(true);
+                    //     setStatut("Ouvert")
+                    // }
+                    // else {
+                    //     setIsOpen(false);
+                    //     setStatut("Fermée")
+                    // }
+
+                    setIsOpen(!isOpen);
+                    setStatut(isOpen ? "Ouvert" : "Fermée");
+                    
+                }} src={arrow} alt="flèche déroulante" className="blockCollapse__btnArrow"></img>
+                <div className="blockCollapse__greybg">
+                    <div className="blockCollapse__text">{text}</div>
+                </div>
         </div>
     )
 }
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -9,10 +9,10 @@ function About(){
         <div className="content">
             <Header />
             <Banner img={mountain} alt={"Montages"}/>
-            <Collapse text="Flabilité"/>
-            <Collapse text="Respect"/>
-            <Collapse text="Service"/>
-            <Collapse text="Sécurité"/>
+            <Collapse title="Flabilité" text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les Photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."/>
+            <Collapse title="Respect" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+            <Collapse title="Service" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+            <Collapse title="Sécurité" text="La sécurité est la priorité de Kasa. AUssi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérfier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
             <Footer />
         </div>
     )
